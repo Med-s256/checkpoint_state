ea9dbdcb3a4923d2b5e261db21fa3d7589ea89a7
Med-s256 <skib.med@gmail.com>
2020-12-23 02:19:33 +0100

    checkpoint state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+
+class App extends Component {
+  state = {
+    Person: {
+      fullName: 'Mohamed Skib', bio: 'my Cv',
+      imgSrc: 'https://thumbs.dreamstime.com/b/portrait-young-man-beard-hair-style-male-avatar-vector-portrait-young-man-beard-hair-style-male-avatar-105082137.jpg', profession: 'technical employee'
+    },show: false, seconds: 0, minutes: 0, hours: 0};
+
+  toggleApp = () => {
+    const currentShow = this.state.show;
+    this.setState({ show: !currentShow })
+  }
+  componentDidMount() {
+    
+    setInterval(() =>{ if(this.state.seconds<59)
+      this.setState((prevsec) => ({
+        seconds: prevsec.seconds+ 1 
+      })) 
+      else 
+      this.setState({seconds:0})
+    }, 1000); 
+    
+    setInterval(() => { if(this.state.minutes<59)
+      this.setState((prevsec) => ({
+
+        minutes: prevsec.minutes+ 1
+      }))
+      else
+      this.setState({minutes:0})
+    }, 60000);
+    setInterval(() => {
+      this.setState((prevsec) => ({
+
+        hours: prevsec.hours + 1
+      }))
+    }, 360000);
+  }
+  render() {
+    
+    let content = null;
+    if (this.state.show) {
+      content = <div>
+        <h1>{this.state.Person.fullName}</h1>
+        <h3>{this.state.Person.bio}</h3>
+        <img src={this.state.Person.imgSrc} width='300px' height='300px' />
+      </div>
+  
+    }
+    return (
+      <div className='App'>
+        <p>{this.state.hours}:{this.state.minutes}:{this.state.seconds}</p>
+        <button onClick={this.toggleApp}>{this.state.show ? 'Hide' : 'Afficher'}</button>
+        {content}
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
